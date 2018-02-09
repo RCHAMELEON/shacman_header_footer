3e8945dc1a469eea83bf80dde32301d65dd16fa6
DealerHTC <dealerxtc@gmail.com>
2018-02-09 22:38:14 +0800

    Init project

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'); 
+var sass = require('gulp-sass'); 
+
+gulp.task('sass', function(){
+	gulp.src('./sass/*.sass')
+	.pipe(sass({outputStyle: 'expanded'}))
+	.pipe(gulp.dest('./sass'));
+});
+
+gulp.task('default', function(){
+	gulp.watch('./sass/*.sass', ['sass']);
+});
